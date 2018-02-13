89949e168a0285109fd73d3d0f3b7b5b9b80ac1f
Polina Rylova <mail@polinarya.ru>
2018-02-13 22:35:04 +0500

    Добавила пример по const

---
+++ constants.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// объявление с помощью const похоже на let,
+// но значение является неизменяемым,
+// поэтому при попытке присвоить другое значение, будет ошибка
+const variable = 'initial value';
+// variable = 'changed value'; // error
