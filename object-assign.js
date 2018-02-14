a07439b866e6c4eb8dcf01f11ff3e7be796b12a2
Polina Rylova <mail@polinarya.ru>
2018-02-14 15:45:11 +0500

    Добавила уточняющий комментарий к Object.assign

--- object-assign.js
+++ object-assign.js
@@ -9,10 +9,11 @@ let obj1 = {a: 1};
 let obj2 = {b: 2};
 let obj3 = {c: 3};
 
-let obj = Object.assign({}, obj1, obj2, obj3);
+let obj = Object.assign({}, obj1, obj2, obj3); // данный метод часто используется для клонирования объектов
 console.log('obj = ' + JSON.stringify(obj)); // копируем поля трёх объектов в новый объект, ссылку на который сохраняем в obj
 // при этом исходные объекты остаются неизменными
 
 let newObj = Object.assign(obj1, obj2, obj3);
 console.log('newObj = ' + JSON.stringify(newObj)); // копируем поля 2-го и 3-го объектов в 1-ый, затем сохраняем ссылку на него в newObj
 console.log('obj1 = ' + JSON.stringify(obj1)); // но при такой реализации меняется объект obj1
+
